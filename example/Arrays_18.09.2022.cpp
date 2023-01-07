371ed1e5af9965d3d85cb59ba8156bbec85a5acf
ObzhalimovEE <evgeniy.Obzhalimov@gmail.com>
2023-01-08 00:49:03 +0500

    Добавьте файлы проекта.

---
+++ example/Arrays_18.09.2022.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<chrono>
+#include <ctime>
+
+/*std::string getTimeStr() {
+	std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::string s(30, '\0');
+	std::strftime(&s[0], s.size(), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+	return s;
+}*/
+int main() {
+	setlocale(LC_ALL, "Russian");
+	/*int size = 10;
+	int* a = new int[size] {22, 3, 44, 555, 6, 7, 666, 7, 77, 6};
+	int b[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		std::cout << "enter the array" << "\n";
+		for (int i = 0; i < size; i++) {
+			std::cout << "a["<<i<<"]="<<a[i] << "; ";
+			}
+		std::cout << "\n";
+		for (int i = 0; i < size; i++) {
+			std::cout << "b[" << i << "]=" << b[i]<<"; ";
+
+		}
+		std::cout << "\n";
+
+		std::cout << a;
+		delete[]a;
+		std::cout << "\n";
+
+		std::cout << b;*/
+	int a;
+	std::cout << a;
+		
+		
+		
+
+
+
+	//Повторение массивов_не домашняя работа
+
+
+
+
+	return 0;
+
+
+}
+
+
